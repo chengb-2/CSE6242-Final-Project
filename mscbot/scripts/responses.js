36db2e16ee489207376268d1a5a2d69f9f97ba60
Zhili Luo <zhilil2@illinois.edu>
2022-04-21 15:45:18 -0400

    Update responses.js

--- mscbot/scripts/responses.js
+++ mscbot/scripts/responses.js
@@ -1,3 +1,6 @@
+const api_path = "https://wuqxv7gtdd.execute-api.us-east-1.amazonaws.com/default/sbf-infer";
+const api_key = "ShufLKMXDa6398N62C5Gi8YYnsoAh8rJa2uKuwwv";
+
 function getBotResponse(input) {
   //rock paper scissors
   console.log("User input: ", input);
@@ -11,6 +14,23 @@ function getBotResponse(input) {
   } else if (input == "goodbye") {
     return "Talk to you later!";
   } else {
-    return "Try sending a sentence!";
+    return userInput(input);
   }
+}
+
+const userInput = async (input) => {
+  const response = await fetch(api_path, {
+    method: 'POST',
+    body: {
+      input: input
+    },
+    headers: {
+      'Content-Type': 'application/json',
+      'x-api-key': api_key
+    }
+  });
+  const res = await response.json(); //extract JSON from the http response
+  console.log(res)
+  // do something with myJson
+  return res.output;
 }
